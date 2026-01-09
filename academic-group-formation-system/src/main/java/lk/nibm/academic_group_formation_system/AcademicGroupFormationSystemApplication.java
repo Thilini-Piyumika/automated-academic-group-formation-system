b89be6451b2adfa4d6d364c29535dfe89989936e
Sahas Suraweera <sahassuraweera11@gmail.com>
2026-01-09 23:11:20 +0530

    Added spring boot project

---
+++ academic-group-formation-system/src/main/java/lk/nibm/academic_group_formation_system/AcademicGroupFormationSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.nibm.academic_group_formation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicGroupFormationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicGroupFormationSystemApplication.class, args);
+	}
+
+}
---
+++ academic-group-formation-system/src/test/java/lk/nibm/academic_group_formation_system/AcademicGroupFormationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.nibm.academic_group_formation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicGroupFormationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
