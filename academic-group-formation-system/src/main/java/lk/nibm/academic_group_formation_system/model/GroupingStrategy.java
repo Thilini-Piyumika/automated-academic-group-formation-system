bd90b23876df8280b7e2cb6edc18239831e057b7
Sahas Suraweera <sahassuraweera11@gmail.com>
2026-01-29 23:56:30 +0530

    Added Grouping Strategy

---
+++ academic-group-formation-system/src/main/java/lk/nibm/academic_group_formation_system/model/GroupingStrategy.java
@@ -0,0 +1,9 @@
+package lk.nibm.academic_group_formation_system.model;
+
+public class GroupingStrategy {
+        BEST_BEST,
+        BEST_AVERAGE,
+        AVERAGE_NEEDS_SUPPORT,
+        NEEDS_SUPPORT_ONLY
+    }
+
