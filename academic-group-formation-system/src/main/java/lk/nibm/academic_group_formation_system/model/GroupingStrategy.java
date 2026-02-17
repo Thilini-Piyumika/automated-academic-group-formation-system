90be00715a1bb35c50c35790b4f6feee9f3e818b
Sahas Suraweera <sahassuraweera11@gmail.com>
2026-02-18 01:08:55 +0530

    Removed old logic

--- academic-group-formation-system/src/main/java/lk/nibm/academic_group_formation_system/model/GroupingStrategy.java
+++
@@ -1,9 +0,0 @@
-package lk.nibm.academic_group_formation_system.model;
-
-public enum GroupingStrategy {
-    BEST_BEST,
-    BEST_AVERAGE,
-    AVERAGE_NEEDS_SUPPORT,
-    NEEDS_SUPPORT_ONLY
-}
-
--- academic-group-formation-system/src/main/java/lk/nibm/academic_group_formation_system/model/LeadershipPreference.java
+++
@@ -1,7 +0,0 @@
-package lk.nibm.academic_group_formation_system.model;
-
-public enum LeadershipPreference {
-    L1, // comfortable taking initiative
-    L2, // can take initiative if needed
-    L3  // prefers guidance
-}
